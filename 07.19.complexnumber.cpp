a4c096ab14cee23da649e43f90ab0b382d19b7fc
GM-10 <137148412+GM-10@users.noreply.github.com>
2023-08-15 19:12:24 +0530

    Update 07.19.complexnumber.cpp

--- 07.19.complexnumber.cpp
+++ 07.19.complexnumber.cpp
@@ -1,4 +1,4 @@
-/*#include<iostream>
+#include<iostream>
 using namespace std;
 struct comp
 {
@@ -8,11 +8,16 @@ int main()
 {
     int i;
     comp a[2];
-    cout<<"Real part\t Imaginary Part"<<endl;
+    cout<<"Enter the real and imaginary part of the complex number."<<endl;
     for(i=0;i<2;i++)
     {
         cout<<"Enter the complex number: "<<i+1<<endl;
         cin>>a[i].real>>a[i].imaginary;
     }
-    
-}*/
+    cout<<"Real part\t Imaginary Part"<<endl;
+    for(i=0;i<2;i++)
+    {
+        cout<<a[i].real<<" + "<<a[i].imaginary<<"i "<<endl;
+    }
+    return 0;    
+}
