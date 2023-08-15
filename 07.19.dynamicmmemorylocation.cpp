6f6b3cb5ad1b0dcdd917bc6c9a0a13c3f874b25d
GM-10 <137148412+GM-10@users.noreply.github.com>
2023-08-15 21:48:27 +0530

    Update 07.19.dynamicmmemorylocation.cpp

--- 07.19.dynamicmmemorylocation.cpp
+++ 07.19.dynamicmmemorylocation.cpp
@@ -11,13 +11,15 @@ int main()
     Student *a[20];
     cout<<sizeof(a)<<endl;
     int i,n;
-    for(i=0;i<5;i++)
+    cout<<"Enter the number of students whose data is need to be taken: ";
+    cin>>n;
+    for(i=0;i<n;i++)
     {
         a[i]= new Student;
         cout<<"Enter rollno, name, class and final score of Student"<<i+1<<endl;
         cin>>a[i]->rno>>a[i]->name>>a[i]->cls>>a[i]->marks;
     }
-    for(i=0;i<5;i++)
+    for(i=0;i<n;i++)
     {
         cout<<"Student "<<i+1<<" : "<<endl;
         cout<<a[i]->name<<" has scored "<<a[i]->marks<<endl;
